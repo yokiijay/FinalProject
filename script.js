78780770ec8c154c9e4920bb56ae07441529a3a0
Yokiijay <yokiijay@gmail.com>
2020-05-11 03:50:21 +0800

    finished

---
+++ script.js
@@ -0,0 +1,36 @@
+/* -------------------------- Examples page -------------------------- */
+
+// swup.on('pageView', ()=>{
+  
+// })
+
+const dropdowns = document.querySelectorAll('#dropdowns')
+
+Array.from(dropdowns).forEach(item=>{
+  const currentDropdown = item
+  const currentContent = currentDropdown.querySelector('.dropdownContent')
+
+  /* -------------------------- 
+    1. 当鼠标移入currentDropdown时
+    2. 检测currentContent底部和右边的边界是否超出屏幕
+    3. 如果超出屏幕，则让content移动到标签上方去
+  -------------------------- */
+
+  currentDropdown.addEventListener('mouseover', ev=>{
+    const contentRect = currentContent.getBoundingClientRect()
+    const maxWidth = window.innerWidth - 15
+    const maxHeight = window.innerHeight - 15
+
+    if(contentRect.bottom > maxHeight && contentRect.right > maxWidth){
+      currentContent.style.transform = `translateY(${-10-currentDropdown.offsetHeight-contentRect.height}px) translateX(${maxWidth-contentRect.right-15}px)`
+    }else {
+      if(contentRect.bottom > maxHeight){
+        currentContent.style.transform = `translateY(${-10-currentDropdown.offsetHeight-contentRect.height}px)`
+      }
+      if(contentRect.right > maxWidth){
+        currentContent.style.transform = `translateX(${maxWidth-contentRect.right-15}px)`
+      }
+    }
+
+  })
+})
